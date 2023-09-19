a53a1e1203a0d0155f5f8bfb4f44d0d929dc36e1
myxik <myxikpupsik@gmail.com>
2023-09-19 23:07:51 +0000

    Rollout buffer written, some optimizations

--- algos/a2c_atari.py
+++ algos/a2c_atari.py
@@ -9,11 +9,11 @@ import numpy as np
 import jax.numpy as jnp
 import gymnasium as gym
 
-from einops import rearrange, repeat
-from collections import deque
+from numba import njit
+
+from einops import rearrange
 from jax import random
 from flax import linen as nn
-from flax.linen.initializers import orthogonal, constant
 from flax.training.train_state import TrainState
 from flax.training import orbax_utils
 from distrax import Categorical
@@ -26,11 +26,11 @@ from stable_baselines3.common.atari_wrappers import (
     NoopResetEnv,
 )
 
-from utils import parse_args
+from utils import parse_args, RolloutBuffer
 
 
 # To enable parallelism for CPU
-os.environ["XLA_FLAGS"] = "--xla_cpu_multi_thread_eigen=true intra_op_parallelism_threads=256"
+os.environ["XLA_FLAGS"] = "--xla_cpu_multi_thread_eigen=true" 
 
 
 class Numpyzi(gym.ObservationWrapper):
@@ -96,13 +96,13 @@ def make_env(env_id, seed, idx):
         return env
     return fn
 
-# --- parAMS ---
+# --- PARAMS ---
 args = parse_args()
 update_every = 5
 algo_name = f"A2C-Atari-{args.env_id}"
 run_name = f"{algo_name}_{time.time()}"
 
-# --- logGER ---
+# --- LOGGER ---
 if args.track:
     wandb.init(
         project=args.project_name,
@@ -114,15 +114,16 @@ if args.track:
     )
 writer = SummaryWriter(f"runs/{run_name}")
 
-# --- rng HANDLE ---
+# --- RNG HANDLE ---
 rng = random.PRNGKey(args.seed)
 key, _ = random.split(rng, 2)
 np.random.seed(args.seed)
 
-# --- env HANDLE ---
+# --- ENV HANDLE ---
 env = gym.vector.SyncVectorEnv([make_env(args.env_id, args.seed+idx, idx) for idx in range(args.num_envs)])
 
 s, _ = env.reset(seed=args.seed)
+s = jnp.asarray(s)
 
 # --- DEFINING AGENTS/NETWORKS ---
 backbone = Network()
@@ -138,7 +139,7 @@ agent_state = TrainState.create(
         critic.init(key, backbone.apply(backbone_params, s)),
     ),
     tx=optax.chain(
-        optax.clip(.5),
+        optax.clip(.1),
         optax.rmsprop(learning_rate=7e-4, initial_scale=1.),
     )
 )
@@ -156,15 +157,12 @@ def update(agent_state, observations, actions, returns):
 
         logprobs = jnp.take_along_axis(logprobs, actions, axis=-1)
 
-        values = jax.lax.stop_gradient(critic.apply(params.critic_params, embed))
-        # values = critic.apply(params.critic_params, embed)
-        scale = returns - values
+        values = critic.apply(params.critic_params, embed)
+        scale = jax.lax.stop_gradient(returns - values)
 
         scaled_logprobs = -logprobs * scale  # actor loss
         actor_loss = jnp.mean(scaled_logprobs, axis=0).squeeze(0)
         
-        embed = backbone.apply(params.backbone_params, observations)
-        values = critic.apply(params.critic_params, embed)
         critic_loss = jnp.mean((returns - values.squeeze(1)) ** 2)  # critic loss
 
         loss = actor_loss + 0.5 * critic_loss
@@ -176,25 +174,13 @@ def update(agent_state, observations, actions, returns):
     return actor_loss, critic_loss, entropy, agent_state
 
 
-def calculate_returns(rewards, dones, gamma):
-    returns = np.zeros(rewards.shape)
-    returns[-1] = rewards[-1]
-    for j in range(returns.shape[1]):  # for each env
-        for i in reversed(range(returns.shape[0] - 1)):
-            returns[i][j] = rewards[i][j] + gamma * (1 - dones[i][j]) * returns[i+1][j]
-    return returns
-
-
 backbone.apply = jax.jit(backbone.apply)
 actor.apply = jax.jit(actor.apply)
 critic.apply = jax.jit(critic.apply)
 
-s, _ = env.reset(seed=args.seed)
-# TODO: Rewrite into own buffer
-buffer = deque(maxlen=update_every)
+rollouts = RolloutBuffer(update_every)
 
 for global_step in range(args.global_steps):
-    # TODO: Rewrite train loop to first collect rollouts then optimize
     for step in range(update_every):
         key, _ = random.split(key, 2)  # handle new random
 
@@ -202,11 +188,10 @@ for global_step in range(args.global_steps):
         logits = actor.apply(agent_state.params.actor_params, backbone.apply(agent_state.params.backbone_params, s))
         dist = Categorical(logits=logits)
         a = dist.sample(seed=key)
-        a = np.asarray(a)  # just to be sure that it is in numpy
 
         s_new, r, term, trun, infos = env.step(a)
 
-        buffer.append((s, a, r, term))  # TODO: rewrite after own buffer is written
+        rollouts.push((s, a, r, term))
 
         s = s_new
 
@@ -219,16 +204,10 @@ for global_step in range(args.global_steps):
                 writer.add_scalar("charts/episodic_return", info["episode"]["r"], global_step)
                 writer.add_scalar("charts/episodic_length", info["episode"]["l"], global_step)
 
-    obs, act, rew, dones = map(jnp.array, zip(*buffer))  # TODO: revisit after buffer
-    last_state_value = critic.apply(agent_state.params.critic_params, backbone.apply(agent_state.params.backbone_params, obs[-1, ...])).squeeze(1)  # TODO: revisit on potential bug
+    # Calculate last value
+    last_value = critic.apply(agent_state.params.critic_params, backbone.apply(agent_state.params.backbone_params, s_new))
 
-    rew = rew.at[-1].set(last_state_value)  # TODO: revisit on potential bug
-    returns = calculate_returns(rew, dones, gamma=args.gamma)
-
-    obs = rearrange(obs, "b env c h w -> (b env) c h w")
-    act = rearrange(act, "b env -> (b env) 1")
-    dones = rearrange(dones, "b env -> (b env) 1")
-    returns = rearrange(returns, "b env -> (b env) 1")
+    obs, act, returns = rollouts.sample(last_value, args.gamma)
 
     actor_loss, critic_loss, entropy, agent_state = update(agent_state, obs, act, returns)
 
--- algos/utils.py
+++ algos/utils.py
@@ -1,4 +1,9 @@
 import argparse
+import numpy as np
+import jax.numpy as jnp
+
+from einops import rearrange
+from collections import deque
 
 
 def parse_args():
@@ -20,3 +25,33 @@ def parse_args():
     parser.add_argument("--entity", help="Wandb entity",
                         default="myxik", type=str, required=False)
     return parser.parse_args()
+
+
+def calculate_returns(last_value, rewards, dones, gamma):
+    returns = np.zeros(rewards.shape)
+    returns[-1] = last_value.squeeze()
+    for j in range(returns.shape[1]):  # for each env
+        for i in reversed(range(returns.shape[0] - 1)):
+            returns[i][j] = rewards[i][j] + gamma * (1 - dones[i][j]) * returns[i+1][j]
+    return returns
+
+
+class RolloutBuffer:
+    def __init__(self, length):
+        self. deque = deque(maxlen=length)
+
+    def push(self, rollout):  # rollout should be in a form of s_i, a_i, r_i, done_i
+        """
+        Rollout tuple should be in form of tuple with state, action, reward and done on this step
+        """
+        self.deque.append(rollout)
+
+    def sample(self, last_value, gamma):
+        obs, act, rew, dones = map(jnp.array, zip(*self.deque))
+        returns = calculate_returns(last_value, rew, dones, gamma)
+
+        obs = rearrange(obs, "b n_envs c h w -> (b n_envs) c h w")
+        act = rearrange(act, "b n_envs -> (b n_envs) 1")
+        returns = rearrange(returns, "b n_envs -> (b n_envs) 1")
+
+        return obs, act, jnp.asarray(returns)
